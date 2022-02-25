ea1dce42e9ae4d7105ee083a2f6065c166b1d0d6
VISHNU J <vibevishnu044@gmail.com>
2022-02-25 21:54:22 +0530

    test commit

--- 3_Implementation/test.c
+++ 3_Implementation/test.c
@@ -1,6 +1,10 @@
 #include "unity.h"
 #include "snake_game.h"
 
+void test_mainMenu(void);
+void test_mainMenu1(void);
+void test_inputScore(void);
+
 void setUp(void){};
 
 void tearDown(void){};
@@ -12,11 +16,20 @@ void test_mainMenu1(void)
 {
     TEST_ASSERT_LESS_OR_EQUAL_INT(1,mainMenu());
 }
+void test_inputScore(void){
+    char highScoreName[20]="Vishnu"
+    int scores[5] = 20;
+    TEST_ASSERT_EQUAL("Vishnu", highScoreName);
+    // TEST_ASSERT_EQUAL(20, scores);
 
+    TEST_ASSERT_EQUAL_INT(20, scores);
+
+}
 int main()
 {
     UNITY_BEGIN();
     RUN_TEST(test_mainMenu);
     RUN_TEST(test_mainMenu);
+    RUN_TEST(test_inputScore);
     return UNITY_END();
 }
